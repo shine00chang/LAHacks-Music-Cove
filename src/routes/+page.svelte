448da452233eee902b6342d5f881edecf16a819d
ShadowAsura <76449821+ShadowAsura@users.noreply.github.com>
2023-04-08 14:52:48 -0700

    stuff
    heeheeheehaw

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,70 @@
-<svelte:head>
-  <link href="/front.css" rel="stylesheet">
-</svelte:head>
-
 <script>
     import Faq from "./faq.svelte";
     import Contact from "./contact.svelte";
-  function create_room() {
-    window.location = "/room/"+document.getElementById("room-name").value+"?create=true";
-  }
-  export let menu;
-</script>
-<div id="bg">
-
-
-  
-<ul id="menu">
-	<li><a href="/" on:click|preventDefault={() => (menu = 1)}>FAQ</a></li> |
-	<li><a href="/" on:click|preventDefault={() => (menu = 2)}>Contact</a></li>
-</ul>
-{#if menu === 1}
-<Faq/>
-{:else if menu === 2}
-<Contact />
-{:else}
-
-{/if}
-<h1>MusicCove</h1>
-
-<form>
-  <h2>Create Room</h2>
-  <input id="room-name" type="text" placeholder="Room name..."/>
-  <button on:click={create_room}>Create</button>
-</form>
-</div>
-<style>
-    div#bg{
-		background: rgb(2,0,36);
-		background: linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(9,102,121,1) 15%, rgba(0,212,255,1) 100%);
+    function create_room() {
+        window.location =
+            "/room/" +
+            document.getElementById("room-name").value +
+            "?create=true";
     }
-    ul#menu li{
-		display : inline;
-	}
+    export let menu;
+</script>
+<main>
+<svelte:head>
+    <link href="/front.css" rel="stylesheet" />
+</svelte:head>
+
+<div id="bg" class="scroll">
+    <ul id="menu">
+        <li><a href="/" on:click|preventDefault={() => (menu = 1)}>FAQ</a></li>
+        |
+        <li>
+            <a href="/" on:click|preventDefault={() => (menu = 2)}>Contact</a>
+        </li>
+    </ul>
+    {#if menu === 1}
+        <Faq />
+    {:else if menu === 2}
+        <Contact />
+    {:else}{/if}
+    <h1>MusicCove</h1>
+
+    <form>
+        <h2>Create Room</h2>
+        <input id="room-name" type="text" placeholder="Room name..." />
+        <button on:click={create_room}>Create</button>
+    </form>
+    <image src="https://cdn.discordapp.com/attachments/1088612557008420864/1094377679362469969/beacch.png"></image>
+</div>
+</main>
+<style>
+    div#bg {
+        background: rgb(2, 0, 36);
+        background: linear-gradient(
+            0deg,
+            rgba(2, 0, 36, 1) 0%,
+            rgba(9, 102, 121, 1) 15%,
+            rgba(0, 212, 255, 1) 100%
+        );
+    }
+    ul#menu li {
+        display: inline;
+    }
     h1 {
         font-family: "Verdana";
-        color:azure;
+        color: azure;
         height: 80px;
         align-self: center;
+    }
+    div.scroll {
+        background-color: #fed9ff;
+        width: 600px;
+        height: 150px;
+        overflow-x: hidden;
+        overflow-y: auto;
+        text-align: center;
+        padding: 20px;
+    }
+    form {
     }
 </style>
--- src/routes/room/[slug]/+page.svelte
+++ src/routes/room/[slug]/+page.svelte
@@ -1,18 +1,14 @@
-<svelte:head>
-  <link href="../../rooms.css" rel="stylesheet">
-</svelte:head>
-
 <script>
   //imports and exports
-  import { page } from '$app/stores';
-  import Start from '$lib/Start.svelte';
-  import E2E from '$lib/e2e.js';
+  import { page } from "$app/stores";
+  import Start from "$lib/Start.svelte";
+  import E2E from "$lib/e2e.js";
   export let data;
   //start stuff
   const ROOM_NAME = data.room_name;
   const SHARED_HASH = $page.url.searchParams.get("shared_hash");
   let create = $page.url.searchParams.get("create") === "true";
-  console.log(ROOM_NAME, create)
+  console.log(ROOM_NAME, create);
   let nickname = "";
   //init E2E class
   let in_room = false;
@@ -28,7 +24,7 @@
   $: {
     if (generate) {
       e2e = new E2E(nickname);
-      console.log('Init E2E')
+      console.log("Init E2E");
       if (create) {
         e2e.send_create_req(ROOM_NAME);
       } else {
@@ -38,10 +34,14 @@
   }
 </script>
 
+<svelte:head>
+  <link href="../../rooms.css" rel="stylesheet" />
+</svelte:head>
+
 <h1>Room: {ROOM_NAME}</h1>
 
 <!--Modal thing that gets nickname and indicates when E2E should be init'd-->
-<Start bind:nickname={nickname} bind:generate={generate}/>
+<Start bind:nickname bind:generate />
 
 {#if in_room}
   <!--If user has the secret key, show them all the components -->
