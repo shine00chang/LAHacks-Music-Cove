29575187e4a412dc891869edc64897433e3ef701
Bhargav Eranki <77950550+beranki@users.noreply.github.com>
2023-04-08 16:45:49 -0700

    i am good kitten~

--- src/routes/room/[slug]/+page.svelte
+++ src/routes/room/[slug]/+page.svelte
@@ -4,10 +4,15 @@
 
 <script>
   //imports and exports
+  import Navbar from "./Navbar.svelte";
+  import Sidebar from "./Sidebar.svelte";
+  let open = false;
+
   import { page } from '$app/stores';
   import Start from '$lib/Start.svelte';
   import E2E from '$lib/e2e.js';
   export let data;
+
   //start stuff
   const ROOM_NAME = data.room_name;
   const SHARED_HASH = $page.url.searchParams.get("shared_hash");
@@ -38,15 +43,16 @@
   }
 </script>
 
-<h1>Room: {ROOM_NAME}</h1>
-
 <!--Modal thing that gets nickname and indicates when E2E should be init'd-->
 <Start bind:nickname={nickname} bind:generate={generate}/>
 
 {#if in_room}
   <!--If user has the secret key, show them all the components -->
-  <h2>YOU IN DA ROOM</h2>
+  <Navbar room_name={ROOM_NAME} bind:sidebar_open={open}/>
+  <Sidebar bind:show={open} />
+
 {:else if generate}
   <!--Show them waiting for approval-->
+  <h1>Room: {ROOM_NAME}</h1>
   <h2>Waiting for Approval...</h2>
 {/if}
---
+++ src/routes/room/[slug]/Sidebar.svelte
@@ -0,0 +1,48 @@
+{#if show}
+  <nav class="navbar-dark bg-dark" transition:fly={{x: 250, opacity: 1}}>
+    <button on:click={() => {member_modal_show = true; show = false;}}>
+      <p style="color: #fff;">View Members</p>
+    </button>
+    <button on:click={() => {game_modal_show = true; show = false;}}>
+      <p style="color: #fff;">Games Menu</p>
+    </button>
+    <button on:click={() => {queue_modal_show = true; show = false;}}>
+      <p style="color: #fff;">Access Queue</p>
+    </button>
+  </nav>
+{/if}
+
+<GameModal bind:show={game_modal_show} />
+<QueueModal bind:show={queue_modal_show} />
+
+<script>
+import { fly } from 'svelte/transition';
+import GameModal from './GameModal.svelte';
+
+
+export let show = false;
+let game_modal_show = false;
+let member_modal_show = false;
+let queue_modal_show = false;
+
+</script>
+		
+<style>
+nav {
+  position: fixed;
+  top: 0;
+  right: 0;
+  height: 100%;
+  padding: 2rem 1rem 0.6rem;
+  border-left: 1px solid #aaa;
+  overflow-y: auto;
+	width: 10rem;
+  margin-bottom: 0.5%;
+}
+
+button {
+  padding: 0;
+  border: none;
+  background: none;
+}
+</style>
