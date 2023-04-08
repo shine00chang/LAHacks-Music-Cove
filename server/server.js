4943a47d38cd4b28f91cadd3a0a9b2246366bc03
Shine Chang <shine00chang@gmail.com>
2023-04-08 10:55:26 -0700

    standardize port

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 import express from "express";
-import dotenv from "dotenv"
+import http from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = 8080;
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+	cors: {
+		origin: CLIENT_URL,
+		methods: ["GET", "POST"]
+	}
+});
 
 app.get('/foo', (req, res) => {
 	console.log("'/foo' requested.");
 	return res.send({hey: "I just met you"});
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log("Listening on port: ", PORT);
 });
