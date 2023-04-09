ae39d6401cd9e93e3ebcf8ca075b8b8b1a712ee2
Bhargav Eranki <77950550+beranki@users.noreply.github.com>
2023-04-09 02:30:47 -0700

    made start.svelte a little nice

--- src/lib/Start.svelte
+++ src/lib/Start.svelte
@@ -15,16 +15,53 @@
 	onsubmit();
   }
 </script>
-
-<div id="start-modal">
-  <div>
-    <h2>Set Nickname</h2>
-    <input bind:value={nickname} placeholder="Nickname here..."/>
-    {#if can_continue}
-      <button on:click={close}>Continue</button>
-    {/if}
+<body style="margin: 0">
+  <div id="start-modal">
+    <div>
+      <h2 class="key-title">Set Nickname</h2>
+      <input class="nickname_input" bind:value={nickname} placeholder="Nickname here..."/>
+      {#if can_continue}
+        <div>
+        <button type="button" class="btn btn-primary" style="margin-top: 100px;" on:click={close}>Continue</button>
+      </div>
+      {/if}
+    </div>
   </div>
-</div>
+</body>
 
 <style>
+    * {
+      font-family: 'Lilita One', cursive;
+      text-align: center;
+    }
+    .key-title {
+      text-align: center;
+      color: #ededed;
+      font-size: 60px;
+      }
+
+    .nickname_input {
+      text-align: center;
+    }
+
+      body {
+        background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+        background-size: 400% 400%;
+        animation: gradient 15s ease infinite;
+        height: 100vh;
+        margin: 20;
+      }
+
+      @keyframes gradient {
+        0% {
+          background-position: 0% 50%;
+        }
+        50% {
+          background-position: 100% 50%;
+        }
+        100% {
+          background-position: 0% 50%;
+        }
+      }
+
 </style>
